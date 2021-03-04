fb7780898661a91c7d9611ec27713092355e5d6b
haeseung-lee <dlgotmd93@gmail.com>
2021-03-05 00:28:55 +0900

    0305...

--- study_API/src/study_API/DateAndCalendar/DateStudy.java
+++ study_API/src/study_API/DateAndCalendar/DateStudy.java
@@ -6,7 +6,7 @@ public class DateStudy {
 
 	public static void main(String[] args) {
 		
-		// 기본생성자로 인스턴스 생성 (기본값 : 지금시간)
+		// 기본생성자로 인스턴스 생성 (기본값 : 시스템 현재시간)
 		Date date = new Date();	
 		System.out.println(date);
 		
--- study_API/src/study_API/string/Application3.java
+++
@@ -1,5 +0,0 @@
-package study_API.string;
-
-public class Application3 {
-
-}
